e826f8b5a00b062d9da8e0b23521e3c0d444b6ce
Bogdan Mihai <bogdanm@fb.com>
2023-04-12 10:05:55 +0200

    contact and newcontact models

---
+++ src/models/contact.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Contact {
+    pub id: ContactId,
+    pub name: String,
+    pub phone_no: i64,
+    pub email: String,
+}
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ContactId(pub i32);
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct NewContact {
+    pub name: String,
+    pub phone_no: i64,
+    pub email: String,
+}
